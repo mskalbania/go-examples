ff4075dbcfe2d5b594341bfb78aa22fd168e37f0
mskalbania <mail@skalbania.it>
2024-09-07 10:23:05 +0200

    Update README.adoc

--- test/benchmark_test.go
+++ test/benchmark_test.go
@@ -1,7 +1,9 @@
 package test
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"strconv"
 	"testing"
 )
@@ -28,5 +30,65 @@ func BenchmarkItoa(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_ = strconv.Itoa(number)
+	}
+}
+
+// 3 functions writing to buffer
+func writeToBufferByVal(buf bytes.Buffer, data []byte) {
+	buf.Write(data)
+}
+
+func writeToBufferByPtr(buff *bytes.Buffer, data []byte) {
+	buff.Write(data)
+}
+
+func writeToWriter(writer io.Writer, data []byte) {
+	writer.Write(data)
+}
+
+func BenchmarkWriteToBufferByVal(b *testing.B) {
+	buf := bytes.Buffer{}
+	data := []byte("data")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		writeToBufferByVal(buf, data)
+	}
+}
+
+func BenchmarkWriteToBufferByPtr(b *testing.B) {
+	buf := new(bytes.Buffer)
+	data := []byte("data")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		writeToBufferByPtr(buf, data)
+	}
+}
+
+func BenchmarkWriteToBufferByPtrWithReset(b *testing.B) {
+	buf := new(bytes.Buffer)
+	data := []byte("data")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buf.Reset()
+		writeToBufferByPtr(buf, data)
+	}
+}
+
+func BenchmarkWriteToWriter(b *testing.B) {
+	buf := new(bytes.Buffer)
+	data := []byte("data")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		writeToWriter(buf, data)
+	}
+}
+
+func BenchmarkWriteToWriterWithReset(b *testing.B) {
+	buf := new(bytes.Buffer)
+	data := []byte("data")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buf.Reset()
+		writeToWriter(buf, data)
 	}
 }
